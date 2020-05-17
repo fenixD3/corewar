df644196575342219a5eeeb95d513779df0f0e3e
Roman Cheremin <romancheremin@iMac-Roman.local>
2020-05-17 20:38:06 +0300

    headers

--- vm/includes/vis_errors.h
+++ vm/includes/vis_errors.h
@@ -1,9 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vis_errors.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcanhand <mcanhand@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/17 14:28:05 by mcanhand          #+#    #+#             */
+/*   Updated: 2020/05/17 14:28:11 by mcanhand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef VIS_ERRORS_H
 # define VIS_ERRORS_H
 
 # define ERR_CREATE_VS "ERROR: while allocating memory for vs struct"
 # define ERR_ML_ARENA "ERROR: while allocating memory for arena"
-# define ERR_ML_CELL "ERROR: while allocating memory for cell of arena"
 
 #endif
